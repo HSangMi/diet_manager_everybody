5314373d2a124691cc21d361c79c52e65c37e8dd
DESKTOP-03TB7OM\joe <lovp576@gmail.com>
2017-09-12 16:21:22 +0900

    갤러리 사진보내기

--- eveybody/www/js/index.js
+++ eveybody/www/js/index.js
@@ -3,8 +3,8 @@ function PhotoInfo(timestamp, data){
     this.data= data;
 }
 var photoList = [];
-var sendPhoto;
 
+// var sendPhoto;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -12,68 +12,138 @@ var app = {
     },
     onDeviceReady: function() {
         console.log("test");
+        var albumIds = [];
 
-        // var recentTime = 1503241200000;
-        sendPhoto = function (recentTime) {
-            var albumIds = [];
-            Photos.collections({"collectionMode": "ALBUMS"},
-                function (albums) {
-                    console.log("앨범", albums);
-                    for (var j = 0; j < albums.length; j++) {
-                        albumIds.push(albums[j].id);
-                    }
-                    Photos.photos(albumIds, {},
-                        function (photos) {
-                            console.log("1111");
-                            var timeList = [];
-                            var urlList = [];
-                            for (var i = 0; i < photos.length; i++) {
+        // if(loginFlag) {
+         function sendPhoto(recentTime, userId) {
+             Photos.collections({"collectionMode": "ALBUMS"},
+                 function (albums) {
+                     console.log("앨범", albums);
+                     for (var j = 0; j < albums.length; j++) {
+                         albumIds.push(albums[j].id);
+                     }
+                     Photos.photos(albumIds, {},
+                         function (photos) {
+                             console.log("1111");
+                             var timeList = [];
+                             var urlList = [];
+                             for (var i = 0; i < photos.length; i++) {
 
-                                if (photos[i].timestamp >= recentTime) {
-                                    var timestamp = (photos[i].timestamp);
-                                    console.log(i, timestamp);
-                                    timeList.push(timestamp);
-                                    Photos.thumbnail(photos[i].id,
-                                        {"asDataUrl": true, "dimension": 300, "quality": 60},
-                                        function (data) {
-                                            urlList.push(data);
-                                            // dataUrls[i] = data;
-                                        },
-                                        function (error) {
-                                            console.error("Error: " + error);
-                                        });
-                                }
-                            }
-                            console.log("2222");
-                            setTimeout(function () {
-                                for (var j = 0; j < urlList.length; j++) {
-                                    photoList.push(new PhotoInfo(timeList[j], urlList[j]));
-                                }
-                                console.dir(photoList);
+                                 if (photos[i].timestamp >= recentTime) {
+                                     var timestamp = (photos[i].timestamp);
+                                     console.log(i, timestamp);
+                                     timeList.push(timestamp);
+                                     Photos.thumbnail(photos[i].id,
+                                         {"asDataUrl": true, "dimension": 300, "quality": 60},
+                                         function (data) {
+                                             urlList.push(data);
+                                             // dataUrls[i] = data;
+                                         },
+                                         function (error) {
+                                             console.error("Error: " + error);
+                                         });
+                                 }
+                             }
+                             console.log("2222");
+                             setTimeout(function () {
+                                 for (var j = 0; j < urlList.length; j++) {
+                                     photoList.push(new PhotoInfo(timeList[j], urlList[j]));
+                                 }
+                                 console.dir(photoList);
 
-                                console.log(JSON.stringify(photoList));
-                                $.ajax({
-                                    url: "http://192.168.0.16:8000/views/utils/sendNewImages.json",
-                                    // url: "http://192.168.0.22:3000/upload",
-                                    data: {"photoList": JSON.stringify(photoList)},
-                                    dataType: "json",
-                                    type: "POST"
-                                }).done(function (result) {
-                                    console.log(result);
-                                }).fail(function (err) {
-                                    console.dir(err);
-                                });
-                            }, 1000);
-                            console.log("3333");
-                        }, console.error);
-                    console.log("44444");
-                    console.log(JSON.stringify(photoList));
+                                 console.log(JSON.stringify(photoList));
+                                 $.ajax({
+                                     url: "http://192.168.0.16:8000/views/utils/sendNewImages.json",
+                                     // url: "http://192.168.0.22:3000/upload",
+                                     data: {
+                                         "photoList": JSON.stringify(photoList),
+                                         userId : userId
+                                     },
+                                     dataType: "json",
+                                     type: "POST",
+                                     crossDomain: true,
+                                     async:false
+                                 }).done(function (result) {
+                                     console.log("성공");
+                                     console.log(result);
+                                     location.replace("view/mybody/myBody.html");
+                                 }).fail(function (err) {
+                                     console.dir(err);
+                                     location.replace("view/mybody/myBody.html");
+                                 });
+                             }, 1000);
+                             console.log("3333");
+                         }, console.error);
+                     console.log("44444");
+                     console.log(JSON.stringify(photoList));
+                 },
+                 function (error) {
+                     console.error("Error: " + error);
+                 });
+         }
+
+        // }
+
+        $("a.loginBtn").click(function(){
+            console.log("로그인 버튼누름");
+
+            var userId = $("input#id").val();
+            var pw = $("input#pw").val();
+
+            $.ajax({
+                url: "http://192.168.0.16:8000/user/login.do",
+                data: {
+                    userId: userId,
+                    pw: pw
                 },
-                function (error) {
-                    console.error("Error: " + error);
-                });
-        };
+                type: "POST",
+                crossDomain: true,
+                async:false
+            }).done(function (result) {
+                if (result === 'fail') {
+                    alert("입력하신 정보가 존재하지 않습니다.");
+                } else {
+                    alert("로그인 성공!");
+                    window.localStorage.setItem("user", userId);
+                    var recentDate = window.localStorage.getItem("recentDate");
 
+                    $(".modal1").click();
+                    if(recentDate !== null && recentDate !== ""){
+                        loginFlag = true;
+                        // 사진 전송
+                        sendPhoto(recentDate, userId);
+                    }
+                }
+            });
+        });
+
+        if(checkMobileDevice()){    // 폰
+            var userId = window.localStorage.getItem("user");
+            var recentDate = window.localStorage.getItem("recentDate");
+            // self.location = "view/mybody/myBody.html";
+
+            console.log(userId);
+            console.log(recentDate);
+
+            // 자동로그인 됐을 때
+            if(userId !== null && userId !== ""){
+
+                // alert("자동로그인상태");
+                // 사진 전송
+                // local의 recentDate와 userId 넘겨주기
+                sendPhoto(1504234130000, userId);
+
+
+            }
+            // 자동로그인 안 됐을 때
+            else{
+                if(recentDate !== null && recentDate !== ""){
+                    $("button#loginModal").trigger('click');
+                }else {
+                    self.location="view/carousel.html";
+                }
+            }
+        }
 
         this.receivedEvent('deviceready');
     },
@@ -89,39 +159,3 @@ var app = {
     }
 };
 app.initialize();
-
-
-
-
-
-
-
-//
-// var app = {
-//     // Application Constructor
-//     initialize: function() {
-//         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-//     },
-//
-//     // deviceready Event Handler
-//     //
-//     // Bind any cordova events here. Common events are:
-//     // 'pause', 'resume', etc.
-//     onDeviceReady: function() {
-//         this.receivedEvent('deviceready');
-//     },
-//
-//     // Update DOM on a Received Event
-//     receivedEvent: function(id) {
-//         var parentElement = document.getElementById(id);
-//         var listeningElement = parentElement.querySelector('.listening');
-//         var receivedElement = parentElement.querySelector('.received');
-//
-//         listeningElement.setAttribute('style', 'display:none;');
-//         receivedElement.setAttribute('style', 'display:block;');
-//
-//         console.log('Received Event: ' + id);
-//     }
-// };
-//
-// app.initialize();
